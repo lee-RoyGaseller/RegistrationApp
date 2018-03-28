f86955d1ef8c219d0aa56caf23003d617ecb9e66
Lee-Roy Gaseller <leeroygaseller@gmail.com>
2018-03-27 19:20:38 -0500

    read from file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.io.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.*;
+
+
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("\n\nHello, RegistrationApp!\n");
+
+        readFromFile("curriculum.dat");
+
+
     }
+
+    private static void readFromFile(String fileName) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+
+        String st;
+        while ((st = br.readLine()) != null) {
+            System.out.println(st);
+        }
+    }
+
 }
