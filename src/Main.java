785b83a05182b69fa10900e029efd6cb6d0625bc
Lee-Roy Gaseller <leeroygaseller@gmail.com>
2018-03-28 20:20:06 -0500

    add hours to list

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.io.*;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintWriter;
 import java.util.*;
 
 
@@ -10,9 +9,29 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         System.out.println("\n\nHello, RegistrationApp!\n");
-
+        ArrayList<Integer> hours = new ArrayList<Integer>();
         readFromFile("curriculum.dat");
+        //add hours to list
+        addHoursToList("Curriculum.dat",hours);
+        System.out.println("total number of hours: "+ hours);
 
+
+
+    }
+
+    private static void addHoursToList(String fileName, ArrayList<Integer> hours) {
+        File infile = new File(fileName);
+        if( ! infile.exists() ) {
+            System.out.println( "Oh no, you can't read from a file that doesn't exist!" );
+        } else {
+            try( Scanner scan = new Scanner( infile ) ) {
+                while( scan.hasNext() ) {
+                    hours.add(scan.nextInt());
+                }
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 
