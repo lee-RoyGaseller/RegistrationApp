d012e1cefdc8ddec8c8e00bd0e9c2cd8e7ee9f38
Lee-Roy Gaseller <leeroygaseller@gmail.com>
2018-04-04 17:54:49 -0500

    Calculate total hours in curriculum

--- src/Main.java
+++ src/Main.java
@@ -25,15 +25,22 @@ public class Main {
         System.out.println("courses: "+ courses);
         System.out.println("courses size: "+ courses.size());
 
-       /* ArrayList<Integer> hours = new ArrayList<>();
-        readFromFile("curriculum.dat");
-        //add hours to list
-        addHoursToList("Curriculum.dat",hours);
-        System.out.println("total number of hours: "+ hours);
-        //add courses to list
-        ArrayList<String> courses = new ArrayList<>();
-        addCoursesToList("Curriculum.dat", courses);*/
+        //calculate total hours in curriculum
+        int totalHours = totalHoursInCurriculum(courses);
+        System.out.println("the total hours in the curriculum: "+ totalHours);
 
+
+
+
+    }
+
+    private static int totalHoursInCurriculum(ArrayList<Course> courses) {
+       int totalHours = 0;
+        for (int i = 0; i <courses.size() ; i++) {
+            totalHours+= courses.get(i).getHours();
+
+        }
+        return totalHours;
 
     }
 
@@ -44,99 +51,43 @@ public class Main {
         } else {
             try( Scanner scan = new Scanner( infile ) ) {
                 while( scan.hasNext() ) {
-                    /*Course course = new Course();
-                    for (int i = 1; i <=3 ; i++) {
-                        if (i ==1){
-                            course.setDept(scan.next());
-                        }
-                        if(i ==2){
-                            course.setCourseNumber(Integer.toString(scan.nextInt()));
-                        }
-                        if (i ==3){
-                            course.setHours(scan.nextInt());
-                        }*/
+
                     String aLine = scan.nextLine();
-                    String[] parts = aLine.split( " " );
+                    String[] parts = aLine.split(" ");
                     System.out.println("\n\nsplit: " + Arrays.asList(parts) + "\n");
-                    if(parts[1].equals("African")){
+                    if (parts[1].equals("African")) {
                         Course course = new Course("African American Heritage", " ", 3);
                         courses.add(course);
 
                     }
-                    /*else {
-                        Course course = new Course(parts[0], parts[1], Integer.parseInt(parts[parts.length-1]));
-                        courses.add(course);
-                    }*/
-                    if(parts[1].equals("Human")){
+
+                    if (parts[1].equals("Human")) {
                         Course course = new Course("Human Past", " ", 3);
                         courses.add(course);
                     }
-                    /*else {
-                        Course course = new Course(parts[0], parts[1], Integer.parseInt(parts[parts.length-1]));
-                        courses.add(course);
-                    }*/
-                    if(parts[1].equals("Scientific")){
+
+                    if (parts[1].equals("Scientific")) {
                         Course course = new Course("Scientific Reasoning", " ", 3);
                         courses.add(course);
                     }
-                    /*else {
-                        Course course = new Course(parts[0], parts[1], Integer.parseInt(parts[parts.length-1]));
-                        courses.add(course);
-                    }*/
-                    if(parts[1].equals("CPSC")){
+
+                    if (parts[1].equals("CPSC")) {
                         Course course = new Course("CPSC Elective", " ", 3);
                         courses.add(course);
                     }
-                    if(parts[0].equals("*")){
-                        
-                    }
-                    else {
+                    if (parts[0].equals("*")) {
+
+                    } else {
 
                         Course course = new Course(parts[0], parts[1], Integer.parseInt(parts[parts.length - 1]));
                         courses.add(course);
                     }
-
-
-                    }
-
+                }
                 } catch (FileNotFoundException e) {
                 e.printStackTrace();
             }
-
         }
         return courses;
-
     }
 
 }
-
-    /*private static void addCoursesToList(String fileName, ArrayList<String> courses) {
-
-    }
-
-    private static void addHoursToList(String fileName, ArrayList<Integer> hours) {
-        File infile = new File(fileName);
-        if( ! infile.exists() ) {
-            System.out.println( "Oh no, you can't read from a file that doesn't exist!" );
-        } else {
-            try( Scanner scan = new Scanner( infile ) ) {
-                while( scan.hasNext() ) {
-                    hours.add(scan.nextInt());
-                }
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
-
-    }
-
-    private static void readFromFile(String fileName) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(fileName));
-
-        String st;
-        while ((st = br.readLine()) != null) {
-            System.out.println(st);
-        }
-    }
-
-}*/
